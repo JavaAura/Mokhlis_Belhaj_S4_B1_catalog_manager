f15465fe2602942e7bf2bbb5ebd059fa550d2498
BelhajMokhlis <belhajmokhlis@gmail.com>
2024-12-08 18:40:09 +0100

    swagger

---
+++ catalog-manager/src/main/java/com/app/catalogmanager/config/OpenApiConfig.java
@@ -0,0 +1,20 @@
+package com.app.catalogmanager.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Catalog Manager API")
+                        .description("API documentation for Catalog Manager application")
+                        .version("v1.0"));
+    }
+} 
